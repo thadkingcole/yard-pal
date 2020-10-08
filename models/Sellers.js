1e81a8d94412aaa4578935518e87de1d804059db
Thad Cole <thadjcole@gmail.com>
2020-10-08 19:23:51 -0400

    create Sellers mongo schema

---
+++ models/Sellers.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const sellerSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    required: "Enter an email",
+    // validation:
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Enter a password",
+    // validation: at least 8 characters?
+  },
+  goal: {
+    type: Number,
+  },
+  revenue: {
+    type: Number,
+  },
+  items: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for the item",
+      },
+      description: {
+        type: String,
+        trim: true,
+      },
+      price: {
+        type: Number,
+        required: "Enter a price for the item",
+        // validation: $XX.XX 2 decimal places
+      },
+      // picture ?
+    },
+  ],
+});
+const Sellers = mongoose.model("Sellers", sellerSchema);
+module.exports = Sellers;
--- server.js
+++ server.js
@@ -4,25 +4,32 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Sellers", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("we're connected!");
+});
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
