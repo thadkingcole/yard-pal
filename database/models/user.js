9bbd98be1b4085a539ec7f01edf622a905845454
Thad Cole <thadjcole@gmail.com>
2020-10-10 14:12:13 -0400

    add validation for price to 2 decimal places

--- database/models/user.js
+++ database/models/user.js
@@ -13,6 +13,7 @@ const userSchema = new Schema({
     required: "Username is required",
     validate: {
       validator: (v) => {
+        // validates 99.99% of emails
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
           v
         );
@@ -26,6 +27,11 @@ const userSchema = new Schema({
     required: "Password is required",
     validate: {
       validator: (v) => {
+        // between 8 & 15 charactors with at least
+        // 1 lowercase letter
+        // 1 uppercase letter
+        // 1 numeric digit
+        // 1 special character
         return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(
           v
         );
@@ -58,6 +64,13 @@ const userSchema = new Schema({
         type: Number,
         required: "Enter a price for the item",
         // validation: $XX.XX 2 decimal places
+        validate: {
+          validator: (v) => {
+            return /^\s*(?=.*[1-9])\d*(?:\.\d{1,2})?\s*$/.test(v);
+          },
+          message: (props) =>
+            `${props.value} is not a valid price (<2 decimals)`,
+        },
       },
       // picture ?
     },
