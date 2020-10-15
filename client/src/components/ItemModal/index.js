5bf6c318eb92866728a105b7edd1c117d40aadcf
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-15 16:09:43 -0400

    routing to back

--- client/src/components/ItemModal/index.js
+++ client/src/components/ItemModal/index.js
@@ -1,10 +1,47 @@
 import React, { useState } from 'react';
 import { Button, Modal, Form, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom'
 
 function ItemModal() {
     const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
+    
     const handleShow = () => setShow(true);
+
+    const history = useHistory();
+
+    const [ newItemInfo, setSetNewItemInfo ] = useState({
+      name: '',
+      description: '',
+      price: 0, 
+    });
+  
+    const handleChange = (event) => {
+    //   const { name, value } = event.target;
+  
+    //   setSetNewItemInfo({ ...newItemInfo, [name]: value });
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setShow(false);
+      axios
+        .put('/api/users:items', {
+          name: newItemInfo.name,
+          description: newItemInfo.description,
+          price: newItemInfo.price
+        })
+        .then((response) => {
+          if (!response.data.error) {
+            history.replace('/Browse');
+          } else {
+            console.log('error: post ItemModal');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
 
     return (
             <Row>
@@ -12,7 +49,7 @@ function ItemModal() {
                     <Button variant="primary" onClick={handleShow}>
                         Add Item
                     </Button>
-                    <Modal show={show} onHide={handleClose}>
+                    <Modal show={show}>
                         <Modal.Header>
                             Add item for Sale
                     </Modal.Header>
@@ -25,6 +62,8 @@ function ItemModal() {
                                         name="newItem-name"
                                         id="newItem-name"
                                         placeholder="Add Item"
+                                        value={newItemInfo.name}
+                                        // onChange={handleChange}
                                     />
                                     <Form.Label for="item">Item Description</Form.Label>
                                     <Form.Control
@@ -32,6 +71,8 @@ function ItemModal() {
                                         name="newItem-description"
                                         id="newItem-description"
                                         placeholder="Add Description"
+                                        value={newItemInfo.description}
+                                        // onChange={handleChange}
                                     />
                                     <Form.Label for="item">Item Price</Form.Label>
                                     <Form.Control
@@ -39,11 +80,13 @@ function ItemModal() {
                                         name="newItem-price"
                                         id="newItem-price"
                                         placeholder="Add Price"
+                                        value={newItemInfo.price}
+                                        // onChange={handleChange}
                                     />
                                     <Button
                                         color="dark"
                                         style={{ marginTop: '2rem' }}
-                                        onClick={handleClose}
+                                        onClick={handleSubmit}
                                     >Add Item</Button>
                                 </Form.Group>
                             </Form>
