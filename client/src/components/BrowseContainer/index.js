e4f85cfaccc1d5e1296dc83ed928a47c8950be94
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-16 10:10:09 -0400

    for bcs call

--- client/src/components/BrowseContainer/index.js
+++ client/src/components/BrowseContainer/index.js
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { Col, Row, Button } from 'react-bootstrap';
 
 function BrowseContainer() {
-const handleClick = () => {
+    const [itemArray, setItemArray] = useState([]);
+    let response;
+    const handleClick = (event) => {
+        event.preventDefault();
+        axios
+        .get('/api/users/browseItems')
+        .then(items => {
+            response = items.data.items;
+            console.log('INSIDE AXIOS', items.data.items);
+            
+        })
+        .catch(err => console.log('get error', err));
+        setItemArray(itemArray => [...itemArray, response])
+        console.log('AFTER setItemArray: ', itemArray);    
 
-    axios
-    .get('/api/users/browseItems')
-    .then(items => {
-        console.log('browse items', items);
-    })
-    .catch(err => console.log('get error', err));
-}
-
+    }
     
-    
-    return(
+    return (
         <Row>
             <Col className="col bg-light p-3 border rounded itemTable">
-            <Row className="m-2 ">
-                <Col className="col bg-white m-1 pt-1 border rounded">
-                    <h4>Image</h4>
-                </Col>
-                <Col className="col bg-white m-1 pt-1 border rounded">
-                    <h4>Item</h4>
-                </Col>
-                <Col className="col bg-white m-1 pt-1 border rounded">
-                    <h4>Description</h4>
-                </Col>
-                <Col className="col bg-white m-1 pt-1 border rounded">
-                    <h4>Price</h4>
-                </Col>
-            </Row>
-            <Row>
-                <Button onClick={handleClick} >Browse Items</Button>
-                
-            </Row>
+                <Row className="m-2 ">
+                    <Col className="col bg-white m-1 pt-1 border rounded">
+                        <h4>Image</h4>
+                    </Col>
+                    <Col className="col bg-white m-1 pt-1 border rounded">
+                        <h4>Item</h4>
+                    </Col>
+                    <Col className="col bg-white m-1 pt-1 border rounded">
+                        <h4>Description</h4>
+                    </Col>
+                    <Col className="col bg-white m-1 pt-1 border rounded">
+                        <h4>Price</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Button onClick={handleClick} >Browse Items</Button>
+                </Row>
             </Col>
         </Row>
     )
