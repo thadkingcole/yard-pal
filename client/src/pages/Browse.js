5ab33e674e31fbd9f3d4298d8f6bf852a7474a4a
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-19 08:23:19 -0400

    commit before work

--- client/src/pages/Browse.js
+++ client/src/pages/Browse.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 import axios from 'axios'
 import ItemModal from '../components/ItemModal/index'
 import BrowseContainer from '../components/BrowseContainer/index'
@@ -10,23 +11,36 @@ function Browse() {
     // Declare itemArray as a setState variable, set to empty array
     const [itemArray, setItemArray] = useState([]);
     //useEffect loads once when page renders calling async fetchData
+    const { userId } = useParams()
+    console.log('user id: ', userId);
 
     useEffect(() => {
-        async function fetchData() {
-            // Async get request from axios
-            const request = await axios
-                .get('/api/users/browseItems');
-            // setItemArray pushes request to itemArray
-            console.log('request browse.js ', request);
-            console.log('index 0: ', request.data[0]);
-            console.log('index 1: ', request.data[1]);
 
+        if (userId) {
 
-           
-            setItemArray(request.data[0]);
-            return request;
+            async function fetchData() {
+                // Async get request from axios
+                const request = await axios
+                    .get(`/api/users/browse/${userId}`);
+                // setItemArray pushes request to itemArray
+                console.log('request (if userId) browse.js ', request);
+                setItemArray(request.data[0]);
+                return request;
+            }
+            fetchData();
+
+        } else {
+            async function fetchData() {
+                // Async get request from axios
+                const request = await axios
+                    .get('/api/users/browseItems');
+                // setItemArray pushes request to itemArray
+                console.log('request browse.js ', request);
+                setItemArray(request.data[0]);
+                return request;
+            }
+            fetchData();
         }
-        fetchData();
         // Calls useEffect anytime itemArray is changed
     }, []);
 
@@ -44,7 +58,8 @@ function Browse() {
                     </Row>
                     <Row>
                         <Col>
-                            
+                            {/* ternary operator switching between either search query for username or map item array, if search for username, 
+                            onClick will pass username into route and return the username which will redirect to browse/:userId), or send please log in*/}
                             {/* {itemArray.map((username, index) => 
                             <p key={index}>{username}</p>
                             )} */}
