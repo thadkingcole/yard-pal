71e226df1e9b5ded57014d4f15f864e5e6dcf06c
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-19 16:39:21 -0400

    cleaned up search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from "react-router-dom";
-import axios from 'axios'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Button } from 'react-bootstrap'
-import { Form } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table';
-import SearchResults from '../components/SearchResults/index';
 import SearchBar from '../components/SearchBar/index';
 
 function Search() {
 
-    // const getItemsById = (userId) => {
-    //     console.log('userId getItemsbyId: ', userId);
-    //     async function fetchData() {
-    //         // Async get request from axios
-    //         await axios
-    //             .get(`/api/users/browse/${userId}`)
-    //             .then(response => {
-    //                 console.log('response.data: ', response.data);
-    //                 items = response.data;
-    //                 console.log('item array', items)
-    //             })
-    //             .catch(err => console.log('catch api/users/browse: ', err));
-    //     }
-    //     fetchData();
-    // }
     return (
         <Container>
             <Row>
@@ -37,11 +16,6 @@ function Search() {
             <Row>
                 <Col className="col-6 border rounded">
                     <SearchBar />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    {/* <SearchResults items={items} /> */}
                 </Col>
             </Row>
         </Container>
