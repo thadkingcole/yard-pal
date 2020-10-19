9fe52cf87eae5483972e71fa6311518960e3c639
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-19 09:07:22 -0400

    styling changes

--- client/src/components/BrowseContainer/index.js
+++ client/src/components/BrowseContainer/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Col from 'react-bootstrap/Col';
 import Row from "react-bootstrap/Row"
 import Button from "react-bootstrap/Button";
+import Table from 'react-bootstrap/Table';
 
 function BrowseContainer({ itemArray, setItemArray }) {
     const handleDelete = (_id) => {
@@ -26,39 +27,50 @@ function BrowseContainer({ itemArray, setItemArray }) {
             });
     }
     return (
-        <>
-            <Row>
-                <Col className="col bg-light p-3 border rounded itemTable">
-                    <Row className="m-2 ">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th><h4>Image</h4></th>
-                                    <th><h4>Item</h4></th>
-                                    <th><h4>Description</h4></th>
-                                    <th><h4>Price</h4></th>
-                                </tr>
-                                {/* Map through Item array into table */}
-                                {itemArray.map((entry, index) =>
-                                    <tr key={index}>
-                                        <td className="entry-img"><img src={entry.imgUrl} alt={entry.name} width="100" height="100"/></td>
-                                        <td className="entry-name">{entry.name}</td>
-                                        <td className="entry-description">{entry.description}</td>
-                                        <td className="entry-price"><h4>$ {entry.price}</h4></td>
-                                        <td>
-                                            <Button
+        <Row>
+            <Col className="col bg-light p-3 border rounded itemTable">
+                <Row className="m-2 ">
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th><h4>Image</h4></th>
+                                <th><h4>Item</h4></th>
+                                <th><h4>Description</h4></th>
+                                <th><h4>Price</h4></th>
+                                <th><h4>Edit/Delete</h4></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* Map through Item array into table */}
+                            {itemArray.map((entry, index) =>
+                                <tr key={index}>
+                                    <td className="entry-img">
+                                        <img
+                                        className="shadow" 
+                                        src={entry.imgUrl} alt={entry.name} width="100" height="100" /></td>
+                                    <td className="entry-name">{entry.name}</td>
+                                    <td className="entry-description">{entry.description}</td>
+                                    <td className="entry-price"><h4>$ {entry.price}</h4></td>
+                                    <td>
+                                        <Button
+                                            className="d-block mx-auto"
+                                            id="delete-btn"
                                             onClick={() => handleDelete(entry._id)}
-                                            >X</Button>
-                                        </td>
-                                    </tr>
+                                        >X</Button>
+                                        <Button
+                                            className="d-block mx-auto mt-2"
+                                            id="edit-btn"
+                                            onClick={() => handleDelete(entry._id)}
+                                        >Edit</Button>
+                                    </td>
+                                </tr>
 
-                                )}
-                            </tbody>
-                        </table>
-                    </Row>
-                </Col>
-            </Row>
-        </>
+                            )}
+                        </tbody>
+                    </Table>
+                </Row>
+            </Col>
+        </Row>
     )
 }
 
